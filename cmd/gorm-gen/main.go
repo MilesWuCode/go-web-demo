b3df3b882b3e544615e46c0140ce1697b567bc22
miles <slion0725@gmail.com>
2026-02-02 15:18:09 +0800

    upd

--- cmd/gorm-gen/main.go
+++ cmd/gorm-gen/main.go
@@ -19,8 +19,16 @@ func main() {
 
 	// 建立 GORM Gen 生成器
 	g := gen.NewGenerator(gen.Config{
-		OutPath: "./generated/query",
-		Mode:    gen.WithoutContext | gen.WithDefaultQuery | gen.WithQueryInterface,
+		OutPath:      "./generated/query",
+		ModelPkgPath: "./model",
+		Mode:         gen.WithoutContext | gen.WithDefaultQuery | gen.WithQueryInterface,
+
+		// 預設值全為false
+		FieldNullable:     true, // 允許指標類型映射 NULL
+		FieldCoverable:    true, // 生成可覆蓋的欄位邏輯
+		FieldSignable:     true, // 支持 unsigned 類型
+		FieldWithIndexTag: true, // 保留索引標籤
+		FieldWithTypeTag:  true, // 保留資料庫具體類型標籤
 	})
 
 	// 建立資料庫連線
