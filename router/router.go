29efc7856833d318df0024c9c5881a8d2f581137
miles <slion0725@gmail.com>
2026-02-03 10:04:34 +0800

    upd

--- router/router.go
+++ router/router.go
@@ -11,24 +11,23 @@ import (
 func NewRouter(app *server.Application) *http.ServeMux {
 	mux := http.NewServeMux()
 
-	// 建立 handler 實例
-	apiHandler := api.NewAPIHandler(app)
-	webHandler := web.NewWebHandler(app)
-
 	// 註冊靜態檔案伺服器
 	dir := http.Dir("./public")
 	fileServer := http.FileServer(dir)
 	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
 
+	// 建立 handler 實例
+	apiHandler := api.NewAPIHandler(app)
+	webHandler := web.NewWebHandler(app)
+
 	// 註冊 API 路由
-	mux.HandleFunc("/api/echo", apiHandler.EchoHandler)
 	mux.HandleFunc("GET /api/users", apiHandler.GetAllUsers)
 	mux.HandleFunc("GET /api/users/{id}", apiHandler.GetUserByID)
 	mux.HandleFunc("GET /api/username/{name}", apiHandler.GetUserByName)
 
 	// 註冊頁面路由
 	mux.HandleFunc("/about", webHandler.AboutHandler)
-	mux.HandleFunc("/", webHandler.HomeHandler) // HomeHandler 作為捕獲所有路由的處理器
+	mux.HandleFunc("/", webHandler.HomeHandler)
 
 	return mux
 }
